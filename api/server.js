cbfd7343314d5a729b5fdf1f10eda4a5a6c48334
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-18 13:17:46 -0500

    put and delete poitns added, delete project deletes all associated actions

--- api/server.js
+++ api/server.js
@@ -30,6 +30,16 @@ server.get('/api/projects/:id', async (req,res) => {
     }
 })
 
+server.get('/api/projects/:id/actions', async (req,res) => {
+    try{
+        let actions = await db.getActions(req.params.id);
+        res.status(200).json(actions);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
 server.post('/api/projects', async (req, res) => {
     try{
         let ids = await db.addProject(req.body);
@@ -40,10 +50,51 @@ server.post('/api/projects', async (req, res) => {
     }
 });
 
-server.post('/api/projects/:id', async (req,res) => {
+server.post('/api/projects/:id/actions', async (req,res) => {
     try{
         let ids = await db.addAction(req.body,req.params.id);
         res.status(201).json(ids)
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+server.put('/api/projects/:id', async (req,res) => {
+    try{
+        let count = await db.updateProject(req.body,req.params.id);
+        res.status(200).json(count);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+server.put('/api/projects/:projectid/actions/:id', async (req,res) => {
+    try{
+        let count = await db.updateAction(req.body,req.params.id);
+        res.status(200).json(count);
+    }
+    catch(err)
+    {
+        res.status(500).json(err);
+    }
+})
+
+server.delete('/api/projects/:id', async (req,res) => {
+    try{
+        let count = await db.removeProject(req.params.id);
+        res.status(200).json(count);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+server.delete('/api/projects/:projectid/actions/:id', async (req,res) => {
+    try{
+        let count = await db.removeAction(req.params.id);
+        res.status(200).json(count);
     }
     catch(err){
         res.status(500).json(err);
--- dataAccess/db.js
+++ dataAccess/db.js
@@ -12,8 +12,31 @@ function addAction(newAction, projectID) {
     return db('actions').insert(newAction);
 }
 
+async function getActions(projectID) {
+    let actions = await db('actions').join('projects','projects.id','=','actions.project_id').select('actions.id','actions.task','actions.notes','actions.complete').where({'projects.id': projectID});
+
+    actions.forEach(action => {
+        if(action.complete === 0){
+            action.complete = false
+        }
+        else{
+            action.complete = true
+        }
+    })
+
+    return actions
+}
+
 function getProjects() {
     return db('projects');
+}
+
+function updateProject(projectInfo, projectID) {
+    return db('projects').where({id: projectID}).update(projectInfo)
+}
+
+function updateAction(actionInfo, actionID) {
+    return db('actions').where({id: actionID}).update(actionInfo)
 }
 
 async function getProject(projectID) {
@@ -42,10 +65,26 @@ async function getProject(projectID) {
         actions
     }
 }
+
+async function removeProject(projectID) {
+    let count = 0;
+    count += await db('projects').where({id: projectID}).del();
+    count += await db('actions').where({project_id: projectID}).del();
+    return count;
+}
+
+function removeAction(actionID) {
+    return db('actions').where({id: actionID}).del();
+}
 //exports
 module.exports = {
     addProject,
     addAction,
     getProjects,
-    getProject
+    getProject,
+    updateProject,
+    updateAction,
+    getActions,
+    removeProject,
+    removeAction
 }
